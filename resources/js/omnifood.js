6fb8cacd77f70c97ad1aeec29ace2179da290b77
Mayank <mayank@Mayanks-MacBook-Air.local>
2017-12-25 15:19:16 +0530

    Completing view of website

--- resources/js/omnifood.js
+++ resources/js/omnifood.js
@@ -1,8 +1,32 @@
-function effect() {
-    const fade = document.querySelector('.init');
-    const topOfNav = fade.offsetTop;
-    if (window.scrollY >= topOfNav-400) {
-        fade.classList.add(['fade-in-anim']);
+function fadeIn() {
+    //feature-anim
+    const feature = document.querySelector('.features');
+    const topOfFeature = 0.85 * feature.offsetTop;
+    const featList = document.querySelector('.feat-list');
+
+    //iphone-anim
+    const hw_it_wrks = document.querySelector('.hw-it-wrk');
+    const topOfhwitwrk = 0.95 * hw_it_wrks.offsetTop;
+    const iphone = document.querySelector('.iphone-img');
+
+    //city-anim
+    const cities = document.querySelector('.cities');
+    const topOfCity = 0.95 * cities.offsetTop;
+    const city = document.querySelector('.city-list');
+
+    if (window.scrollY >= topOfFeature) {
+        featList.classList.add(['fade-in-anim']);
+    }
+
+    if (window.scrollY >= topOfCity) {
+        city.classList.add(['city-anim']);
+    }
+
+    if (window.scrollY >= topOfhwitwrk) {
+        iphone.classList.add(['iphone-anim']);
     }
 }
-window.addEventListener('scroll', effect);
+window.addEventListener('scroll', fadeIn);
+
+//var c = document.getElementsByTagName('section')
+// var c = document.getElementsByTagName('header')
